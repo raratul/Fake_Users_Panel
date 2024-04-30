ed37f57b536204ea0fc040ef2d9dd5017de92cae
raratul <rkhairulislam@gmail.com>
2024-05-01 00:13:50 +0600

    updated files.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,8 +7,8 @@ chmod +x bin/rails
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails
+# bundle exec rails
 
-bin/rails db:environment:set RAILS_ENV=production 
-rake DISABLE_DATABASE_ENVIRONMENT_CHECK=1 db:migrate:reset
-RAILS_ENV=test rake db:migrate
+# bin/rails db:environment:set RAILS_ENV=production 
+# rake DISABLE_DATABASE_ENVIRONMENT_CHECK=1 db:migrate:reset
+# RAILS_ENV=test rake db:migrate
